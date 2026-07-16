a1de366e8c4763aab71b8daf87b6df318a67cb81
Jishco <jish.strong@gmail.com>
2026-07-16 21:24:27 +0000

    update dockerfile and files

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,15 +1,14 @@
 import sys
-print ("arguments", sys.argv)
+
+import pandas as pd 
+#print ("arguments", sys.argv)
 
 day = int(sys.argv[1])
 print(f"Running pipeline for day {day}")
 
 
-
-import pandas as pd 
-
 df = pd.DataFrame ({"A": [1,2],  "B": [3,4]})
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
-
+print(f"Completed pipeline for day {day}")
